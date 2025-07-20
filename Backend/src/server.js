397ed14fe13ed9571c31cd0f29b4396093317d43
Ranjith Kumar Reddy <p.ranjithkumar200517@gmail.com>
2025-07-20 14:23:34 +0530

    third changes

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/noteRoute.js"
 import { connectDB } from "./config/db.js";
@@ -8,16 +9,28 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 // console.log(process.env.MONGO_URI);
 const app=express()
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+const __dirname=path.resolve();
 connectDB();
+
+
+if(process.env.NODE_ENV!=="production"){
 app.use(cors(
     {origin:"http://localhost:5173" ,}
 ))
+}
+
 app.use(express.json())
 app.use(rateLimiter)
 
-app.use("/api/notes",notesRoutes)
+app.use("/api/notes",notesRoutes);
 
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+});
+}
 
 
 app.listen(PORT, ()=>{
